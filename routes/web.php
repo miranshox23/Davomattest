7e3236d15db649a94b77917e19b8d0a2c5cb429e
MIRONSHOKH ABDURAKHIMOV <77046390+Miranshox@users.noreply.github.com>
2022-10-08 20:04:42 +0500

    -m

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::middleware('auth')->group(function () {
